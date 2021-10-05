4133ee4e73be25f892a0975a542ab5dbd2a72630
Julian Dos Santos <dossantos38529@estudiantes.untref.edu.ar>
2021-10-05 15:58:38 -0300

    Initial commit

---
+++ src/brains/MyBrain.java
@@ -0,0 +1,34 @@
+package brains;
+
+import java.util.List;
+
+import edu.unlam.snake.brain.Brain;
+import edu.unlam.snake.engine.Direction;
+import edu.unlam.snake.engine.Point;
+
+public class MyBrain extends Brain {
+
+	// Pueden agregarse todos los atributos necesarios
+	
+	public MyBrain() {
+		super("COMPLETAR NOMBRE DE EQUIPO");
+	}
+
+	/**
+	 * Retorna el próximo movimiento que debe hacer la serpiente.
+	 * @param head, la posición de la cabeza
+	 * @param previous, la dirección en la que venía moviéndose
+	 */
+	public Direction getDirection(Point head, Direction previous) {
+		List<Point> fruits = info.getFruits();
+		List<Point> snake = info.getSnake();
+		List<List<Point>> enemies = info.getEnemies();
+		List<Point> obstacles = info.getObstacles();
+
+		// completar con la lógica necesaria para mover la serpiente,
+		// intentando comer la mayor cantidad de frutas y sobrevivir
+		// el mayor tiempo posible.
+		
+		return previous.turnRight();
+	}
+}
