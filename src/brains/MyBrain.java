cde9932f96e0c8245675c200b41989516668b6c2
Julian Dos Santos <dossantos38529@estudiantes.untref.edu.ar>
2021-10-05 17:31:27 -0300

    agregada la logica base para saber si hay peligro al rededor del punto

--- src/brains/MyBrain.java
+++ src/brains/MyBrain.java
@@ -1,7 +1,7 @@
 package brains;
 
 import java.util.List;
-
+import java.util.Iterator;
 import edu.unlam.snake.brain.Brain;
 import edu.unlam.snake.engine.Direction;
 import edu.unlam.snake.engine.Point;
@@ -25,10 +25,46 @@ public class MyBrain extends Brain {
 		List<List<Point>> enemies = info.getEnemies();
 		List<Point> obstacles = info.getObstacles();
 
+		 Direction nextDirection = previous;
+	      Point nextPoint = head.clone();
+	      nextPoint.moveTo(previous);
 		// completar con la lógica necesaria para mover la serpiente,
 		// intentando comer la mayor cantidad de frutas y sobrevivir
 		// el mayor tiempo posible.
-		previous.turnLeft();
-		return previous.turnRight();
+		
+		return nextDirection;
+	}
+	
+	// devuelve true si hay un enemigo, una pared o una parte de la misma snake
+	
+	private boolean hayPeligro(Point point){
+		
+		// primero busco si el punto al que voy a ir es una parte de la serpiente
+		
+		for(Point cuerpo : info.getSnake()){
+			if(cuerpo.equals(point)){
+				return true;
+			}
+		}
+		
+		// despues busco si en el punto al que voy  hay un enemigo	
+		for(List<Point> enemigos : info.getEnemies()){
+			for (Point enemigo : enemigos) {
+				if(enemigo.equals(point)){
+					return true;
+				}
+			}
+		}
+		
+		// me fijo si al punto al que voy hay un obstaculo
+		
+		for(Point obst:info.getObstacles()){
+			if(obst.equals(point)){
+				return true;
+			}
+		}
+		
+		
+		return false;
 	}
 }
